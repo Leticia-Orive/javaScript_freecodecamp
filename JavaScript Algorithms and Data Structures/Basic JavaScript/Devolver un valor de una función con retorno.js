adf39518b51c17e5bfdd39658eff552a9dd387e8
LETICIA ORIVE PEÑA <leticia.orive.contractor@bbva.com>
2023-04-12 09:15:31 +0200

    Funciones  JavaScript

---
+++ JavaScript Algorithms and Data Structures/Basic JavaScript/Devolver un valor de una función con retorno.js
@@ -0,0 +1,32 @@
+//Devolver un valor de una función con retorno
+/* Podemos pasar valores a una función con argumentos . Puede usar una returndeclaración para enviar un valor de vuelta fuera de una función.
+
+Ejemplo
+
+function plusThree(num) {
+  return num + 3;
+}
+
+const answer = plusThree(5);
+answertiene el valor 8.
+
+plusThreetoma un argumento a favor numy devuelve un valor igual a num + 3.
+
+Cree una función timesFiveque acepte un argumento, lo multiplique por 5y devuelva el nuevo valor.*/
+//Ejercicio
+/* timesFive debería ser una función
+timesFive(5) debería volver25
+timesFive(2) debería volver10
+timesFive(0) debería volver0 */
+
+function minusSeven(num) {
+  return num - 7;
+}
+
+console.log(minusSeven(10));
+
+function timesFive(num) {
+  return num * 5;
+}
+
+console.log(timesFive(5));
---
+++ JavaScript Algorithms and Data Structures/Basic JavaScript/Pasar valores a funciones con argumentos.js
@@ -0,0 +1,22 @@
+//Pasar valores a funciones con argumentos
+/* Los parámetros son variables que actúan como marcadores de posición para los valores que se deben ingresar a una función cuando se la llama. Cuando se define una función, normalmente se define junto con uno o más parámetros. Los valores reales que se ingresan (o "pasan" ) en una función cuando se llama se conocen como argumentos .
+
+Aquí hay una función con dos parámetros, param1y param2:
+
+function testFun(param1, param2) {
+  console.log(param1, param2);
+}
+Entonces podemos llamar testFunasí: testFun("Hello", "World");. Hemos pasado dos argumentos de cadena, Helloy World. Dentro de la función, param1será igual a la cadena Helloy param2será igual a la cadena World. Tenga en cuenta que podría testFunvolver a llamar con diferentes argumentos y los parámetros tomarían el valor de los nuevos argumentos.
+
+Cree una función llamada functionWithArgsque acepte dos argumentos y envíe su suma a la consola de desarrollo.
+Llame a la función con dos números como argumentos.*/
+//Ejercicio
+/* functionWithArgs debe ser una función.
+functionWithArgs(1,2) debe dar salida 3.
+functionWithArgs(7,9) debe dar salida 16.
+Debe llamar functionWithArgscon dos números después de definirlo.*/
+function functionWithArgs(a, b) {
+  console.log(a + b);
+}
+
+functionWithArgs(10, 5);
