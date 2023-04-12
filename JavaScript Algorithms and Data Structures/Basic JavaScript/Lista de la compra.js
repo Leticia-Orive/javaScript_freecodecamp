1781354475eaf4666e370e9cab33f5af6ed07b87
LETICIA ORIVE PEÑA <leticia.orive.contractor@bbva.com>
2023-04-12 08:59:55 +0200

    Matrices  JavaScript

---
+++ JavaScript Algorithms and Data Structures/Basic JavaScript/Lista de la compra.js
@@ -0,0 +1,19 @@
+//Lista de la compra
+/* Crea una lista de compras en la variable myList. La lista debe ser una matriz multidimensional que contenga varias sub-matrices.
+
+El primer elemento de cada subarreglo debe contener una cadena con el nombre del elemento. El segundo elemento debe ser un número que represente la cantidad, es decir
+
+["Chocolate Bar", 15]
+Debe haber al menos 5 sub-matrices en la lista.*/
+//Ejercicio
+/* myList debe ser una matriz.
+Los primeros elementos en cada una de sus sub-matrices deben ser cadenas.
+Los segundos elementos en cada una de sus sub-matrices deben ser todos números.
+Debes tener al menos 5 artículos en tu lista.*/
+const myList = [
+  ["cereal", 3],
+  ["milk", 2],
+  ["bananas", 3],
+  ["juice", 2],
+  ["eggs", 12],
+];
